08024351c6a73372413249774ed7e199c950aa1b
karlofff24 <120955101+karlofff24@users.noreply.github.com>
2025-09-24 02:21:51 +0900

    исправления

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
+
 def add(a, b):
     return a + b
 
-def subtract(a, b):   
-    return a - b       
+def substract(a, b):   
+    return a + b       
+
 def multiply(a, b):
     return a * b
 
 def divide(a, b):
-    if b == 0:
-        return "Error: Division by zero!"  
-    return a / b
+    return a / b   
 
 def main():
     print("Simple Calculator")
-    x = int(input("Enter first number: "))
-    y = int(input("Enter second number: "))
+    
+    while True:  
+        try:
+            x = float(input("Enter first number: "))  
+            break  
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    
+    while True:  
+        try:
+            y = float(input("Enter second number: "))  
+            break  
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
     print("Addition:", add(x, y))
-    print("Subtraction:", subtract(x, y)) 
+    print("Subtraction:", substract(x, y))   
     print("Multiplication:", multiply(x, y))
-    print("Division:", divide(x, y))       
+    print("Division:", divide(x, y))         
+
 if __name__ == "__main__":
     main()
